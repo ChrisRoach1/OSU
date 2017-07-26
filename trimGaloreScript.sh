01f85eabc4cd1c42dfccfa463999ead9f7bf3c3f
ChrisRoach1 <c.roach10@yahoo.com>
2017-07-26 14:39:02 -0400

    Update trimGaloreScript.sh

--- trimGaloreScript.sh
+++ trimGaloreScript.sh
@@ -13,6 +13,8 @@ cd
 #its important to note that this could possibly lead to reads being completely empty that 
 #could cause problems down the line for alignment, may need to trim again and cut out the 
 #empty pairs
+# example used to get rid of empty reads: ./TrimGalore-0.4.3/trim_galore -length 12 SRAGATA2_trimmed.fq 
+#alternatively you can just change the original code and make (-length 0) into (-length 12), so there are less steps
 
 
 
